8fe37e219478fc4169ca764164e0cba4bb572f23
ranjiT #Learner <ranjithkumar2010a@live.com>
2015-08-22 01:08:13 +0530

    Initial Commit

---
+++ AddressBookIntegration/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AddressBookIntegration
+//
+//  Created by ranjit on 22/08/15.
+//  Copyright © 2015 ranjit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AddressBookIntegration/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AddressBookIntegration
+//
+//  Created by ranjit on 22/08/15.
+//  Copyright © 2015 ranjit. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
